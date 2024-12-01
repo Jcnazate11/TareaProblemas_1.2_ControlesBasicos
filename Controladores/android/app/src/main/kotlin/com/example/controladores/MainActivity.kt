3129f75a275bd95138bdfa967145d74e089c21bb
Jcnazate11 <jcnazate@espe.edu.ec>
2024-12-01 15:05:28 -0500

    cambio

---
+++ Controladores/android/app/src/main/kotlin/com/example/controladores/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.controladores
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/example/eje1_intercambio/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.eje1_intercambio
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
